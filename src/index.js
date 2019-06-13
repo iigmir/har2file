5677c7947ddfd6bd5822714deab612ff6144ebd0
iigmir <roc120j@gmail.com>
2019-06-13 18:15:28 +0800

    Add ignores

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const render_har = require("./render_har");
+
 module.exports =  function(err, data)
 {   // Read the file and print its contents.
     let parsed_json = {};
@@ -9,5 +11,5 @@ module.exports =  function(err, data)
         throw new Error("Not a vaild HAR file.");
     }
     // console.log("OK: " + filename);
-    console.log(parsed_json);
+    render_har(parsed_json);
 };
---
+++ src/render_har.js
@@ -0,0 +1,4 @@
+module.exports = function(har)
+{
+    debugger;
+}
