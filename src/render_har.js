46e100ce49ac9df4a80e2cdaccd64dfd974e658e
iigmir <roc120j@gmail.com>
2019-06-13 18:33:25 +0800

    Add render har

--- src/render_har.js
+++ src/render_har.js
@@ -1,4 +1,24 @@
 module.exports = function(har)
 {
-    debugger;
+
+    let files = har.log.entries;
+    files.map( file =>
+    {
+        let response = file.response;
+        let response_success = response.status === 200;
+        let file_positison = file.request.url.split("/");
+        let content = "";
+        if( response_success )
+        {
+            content = response.content;
+            debugger;
+        }
+        else
+        {
+            let warn_msg = `Warning: The file ${ file.request.url } is not vaild because HTTP code is ${ response.status }`;
+            console.warn(warn_msg);
+            debugger;
+        }
+    });
+    
 }
