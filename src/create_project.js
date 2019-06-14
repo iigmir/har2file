f4fd4818b17f00a4956a39a3f1bcb481f0594cb1
iigmir <roc120j@gmail.com>
2019-06-14 15:13:43 +0800

    Seprate create

---
+++ src/create_project.js
@@ -0,0 +1,23 @@
+/**
+ * Create following directory:
+ * /dist/foo
+ * /dist/foo2
+ * /dist/foo3
+ * /dist/foo4
+ * ... And so on
+ * @param {string} name - Project name
+ * @param {string} number - If project exists, increse number that will be useful.
+ * @param {function} callback - If create success, call this param function.
+ */
+module.exports = ({ name, number, callback }) =>
+{
+    let temp_number = number + 1;
+    let tmep_str = temp_number > 1 ? String( temp_number ) : "";
+    let dist_path = name + tmep_str;
+    if( fs.existsSync(dist_path) )
+    {
+        return create_project({ name, number: temp_number });
+    }
+    fs.mkdir( dist_path, { recursive: true }, err => callback(err) );
+    return;
+};
--- src/write_files.js
+++ src/write_files.js
@@ -11,41 +11,10 @@ module.exports = function({ file_positison, mime_type, content_text })
 {
     // file_positison will filter "http(s)", "", "example.com" .slice(3)
     let project_name = file_positison[2];
-    /**
-     * Create following directory:
-     * /dist/foo
-     * /dist/foo2
-     * /dist/foo3
-     * /dist/foo4
-     * ... And so on
-     * @param {string} name - project name
-     * @param {string} number - If project exists, increse number that will be useful.
-     */
-    let create_project = ({ name, number }) =>
-    {
-        let dist_path = "/dist/" + name;
-        let temp_number = number + 1;
-        if( fs.existsSync(dist_path) )
-        {
-            return create_project({ name, number: temp_number });
-        }
-        // fs.mkdirSync(dist_path, 0777);
-        fs.mkdir(
-            dist_path,
-            { recursive: true },
-            err => create_files({
-                file_positison,
-                mime_type,
-                content_text,
-                create_project_error: err
-            })
-        );
-        return;
-    };
-    let create_files = ({ create_project_error }) =>
+    
+    let create_files_func = ({ create_project_error }) =>
     {
         if (create_project_error) throw create_project_error;
-        debugger;
     };
-    create_project({ name: project_name, number: 1 });
+    create_project({ name: "dist/" + project_name, number:0, callback: create_files_func({ create_project_error: err }) });
 };
