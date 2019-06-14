f0c6d570f1adbb3beb52c6ff079389b3ba414af0
iigmir <roc120j@gmail.com>
2019-06-14 14:22:54 +0800

    Add create_project

--- src/write_files.js
+++ src/write_files.js
@@ -9,6 +9,28 @@ const fs = require("fs");
 module.exports = function({ file_positison, mime_type, content_text })
 {
     // file_positison will filter "http(s)", "", "example.com" .slice(3)
+    let project_name = file_positison[2];
+    /**
+     * Create following directory:
+     * /dist/foo
+     * /dist/foo2
+     * /dist/foo3
+     * /dist/foo4
+     * ... And so on
+     * @param {string} name - project name
+     * @param {string} number - If project exists, increse number that will be useful.
+     */
+    let create_project = ({ name, number }) =>
+    {
+        let dist_path = "/dist/" + name;
+        let temp_number = number + 1;
+        if( fs.existsSync(dist_path) )
+        {
+            return create_project({ name, number: temp_number });
+        }
+        return dist_path;
+    };
+    create_project({ name: project_name, number: 1 });
     // let path = "./dst/foo/bar.txt";
     // if( !fs.existsSync(path) )
     // {
