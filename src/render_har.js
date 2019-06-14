b1efef24494a1dfebe1d68ee7024fda512e3caa3
iigmir <roc120j@gmail.com>
2019-06-14 13:53:14 +0800

    Rewrite MMIE

--- src/render_har.js
+++ src/render_har.js
@@ -1,4 +1,5 @@
 const base64_decode = require("./base64_decode");
+const write_files = require("./write_files");
 
 module.exports = function(har)
 {
@@ -8,31 +9,33 @@ module.exports = function(har)
     {
         let response = file.response;
         let response_success = response.status === 200;
+        // file_positison will filter "http(s)", "", "example.com" .slice(3)
         let file_positison = file.request.url.split("/");
         let content = {};
-        let mime_type = [];
-        let text = "";
-        let should_encoded = false;
-        let decoded_contect;
+        let mmie_info = { type:[], should_encoded: false };
+        let content_text = "";
+        
         if( response_success )
         {
             content = response.content;
-            mime_type = content.mimeType.split(";")[0].split("/");
-            should_encoded = content.hasOwnProperty("encoding");
-            if( should_encoded )
+            mmie_info = {
+                type: content.mimeType.split(";")[0].split("/"),
+                should_encoded: content.hasOwnProperty("encoding")
+            };
+            if( mmie_info.should_encoded )
             {
                 if( content.encoding !== "base64" )
                 {
                     console.error( content.encoding );
                     throw new Error( "No suitable decode type." );
                 }
-                text = base64_decode( content.text );
+                content_text = base64_decode( content.text );
             }
             else
             {
-                text = content.text;
+                content_text = content.text;
             }
-            debugger;
+            write_files({ file_positison, mmie_info, content_text });
         }
         else
         {
---
+++ src/write_files.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+// let path = "./dst/foo/bar.txt";
+
+// if( !fs.existsSync(path) )
+// {
+//     fs.mkdirSync(path, 0744);
+// }
+
+// fs.writeFile( path , "<contenet>" , err =>
+// {
+//     if (err) throw err;
+//     console.log('File is created successfully.');
+// });
+
+module.exports = function({ file_positison, mime_type, content_text })
+{
+    //
+}
